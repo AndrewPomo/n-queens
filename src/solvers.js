03a5617bd30d86d82aa986564b7d6da1adf585e9
andrewpomo <andrewpomo815@gmail.com>
2018-03-15 19:31:25 -0700

    Complete n Rooks

--- src/solvers.js
+++ src/solvers.js
@@ -13,26 +13,12 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-window.createMatrix = function(n) {
-  var matrix = [];
-  for(var i = 0; i < n; i++) {
-    var row = new Array(n);
-    row.fill(0, 0, n);
-    matrix.push(row);
-  }
-  return matrix;
-};
-
 window.findNRooksSolution = function(n) {
   var matrix = new Board({n:n});
   
-  // if (n === 2) {
-  //   debugger;
-  // }
-  
-  var permutate = function(rooks) {
+  var permutate = function(row) {
     var rows = matrix.rows();
-    if (rooks === 0) {
+    if (row === n) {
       var resultArr = [];
       for (var key in matrix.attributes) {
         resultArr.push(matrix.attributes[key]);
@@ -40,21 +26,16 @@ window.findNRooksSolution = function(n) {
       resultArr.pop();
       return resultArr;
     }
-    // debugger;
-    for (var row = 0; row < n; row++) {
-      for (var col = 0; col < n; col++) {
-        if(rows[row][col] !== 1) {
-          matrix.togglePiece(row, col);
-          if (!matrix.hasAnyRooksConflicts()) {
-            return permutate(rooks - 1);
-          }
-          matrix.togglePiece(row, col);
-        } 
+    for (var col = 0; col < n; col++) {
+      matrix.togglePiece(row, col);
+      if (!matrix.hasAnyRooksConflicts()) {
+        return permutate(row + 1);
       }
+      matrix.togglePiece(row, col);
     }
   };
   
-  solution = permutate(n);
+  var solution = permutate(0);
   
   
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
@@ -74,13 +55,11 @@ window.countNRooksSolutions = function(n) {
       return;
     }
     for (var col = 0; col < n; col++) {
-      if(rows[row][col] !== 1) {
-        matrix.togglePiece(row, col);
-        if (!matrix.hasAnyRooksConflicts()) {
-          permutate(row + 1);
-        }
-        matrix.togglePiece(row, col);
-      } 
+      matrix.togglePiece(row, col);
+      if (!matrix.hasAnyRooksConflicts()) {
+        permutate(row + 1);
+      }
+      matrix.togglePiece(row, col);
     }
   };
   
@@ -92,18 +71,55 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var matrix = this.createMatrix(n);
+  var matrix = new Board({n:n});
+  var permutate = function(row) {
+    var rows = matrix.rows();
+    if (row === n) {
+      var resultArr = [];
+      for (var key in matrix.attributes) {
+        resultArr.push(matrix.attributes[key]);
+      }
+      resultArr.pop();
+      return resultArr;
+    }
+    for (var col = 0; col < n; col++) {
+      matrix.togglePiece(row, col);
+      if (!matrix.hasAnyQueensConflicts()) {
+        return permutate(row + 1);
+      }
+      matrix.togglePiece(row, col);
+    }
+  };
+  
+  var solution = permutate(0);
   
   
-
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  var solutionCount = 0; //fixme
+  var matrix = new Board({n:n});
+  
+  var permutate = function(row) {
+    var rows = matrix.rows();
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+    for (var col = 0; col < n; col++) {
+      matrix.togglePiece(row, col);
+      if (!matrix.hasAnyQueensConflicts()) {
+        permutate(row + 1);
+      }
+      matrix.togglePiece(row, col);
+    }
+  };
+  
+  permutate(0);
+  
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
